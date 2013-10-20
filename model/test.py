c2af3095b846ae192ff4ed48593ab9f35e96e04a
Tom Badran <tom@badrunner.net>
2013-10-20 20:19:34 +0100

    Generate figures for report

--- model/test.py
+++ model/test.py
@@ -14,31 +14,36 @@ from uniform_disk import uniform_disk_mandel, uniform_disk
 
 if __name__ == '__main__':
     # Simulation of large hot jupiter orbiting a star
+    d = 0.2 * c.au.value
+    rp = 2 * c.R_jup.value
+    mp = c.M_jup.value
+    rs = c.R_sun.value
+    ms = c.M_sun.value
+    n = 10000
     t_factor = 0.008
-    t, uniform = uniform_disk_mandel(0.2 * c.au.value, 2 * c.R_jup.value,
-                                     c.M_jup.value, c.R_sun.value, c.M_sun.value,
-                                     1000, t_factor)
 
-    _, limb = quadratic_mandel(0.2 * c.au.value, 2 * c.R_jup.value,
-                               c.M_jup.value, c.R_sun.value, c.M_sun.value,
-                               1000, t_factor)
+    t, uniform = uniform_disk_mandel(d, rp, mp, rs, ms, n, t_factor)
+    _, limb = quadratic_mandel(d, rp, mp, rs, ms, n, t_factor)
+    _, dumb = uniform_disk(d, rp, mp, rs, ms, n, t_factor)
 
-    _, dumb = uniform_disk(0.2 * c.au.value, 2 * c.R_jup.value,
-                           c.M_jup.value, c.R_sun.value, c.M_sun.value,
-                           1000, t_factor)
-
-    t = t + abs(t.min())
-    plt.plot(t, uniform, 'b', linestyle='--', label='Uniform')
-    plt.plot(t, limb, 'r', label='Limb-Dark')
-    plt.plot(t, dumb, 'g', linestyle='-.', label='My Model')
-
-    plt.legend(loc='best')
-    plt.ylim(0.94, 1.005)
+    t = (t + abs(t.min())) / 60
+    plt.plot(t, dumb, 'k')
+    plt.ylim(dumb.min() * 0.99, dumb.max() * 1.01)
     plt.xlim(t.min(), t.max())
-    plt.title('Transit Modelling')
     plt.xlabel('Time / minutes')
     plt.ylabel('Relative Observed Flux')
+    plt.savefig('../report/images/uniform_disk_model.pdf')
 
+    plt.figure()
+    #plt.plot(t, uniform, 'b', linestyle='--', label='Uniform')
+    plt.plot(t, limb, 'k', linestyle='-.', label='Quadratic Limb Darkening')
+    plt.plot(t, dumb, 'k', linestyle='-', label='Uniform Disk')
 
+    plt.legend(loc='best')
+    plt.ylim(0.93, 1.005)
+    plt.xlim(t.min(), t.max())
+    plt.xlabel('Time / minutes')
+    plt.ylabel('Relative Observed Flux')
+    plt.savefig('../report/images/model_comparison.pdf')
 
     plt.show()
